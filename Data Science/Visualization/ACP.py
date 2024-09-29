c93ed8d9fa0271083a7919dd9b05051d7b306778
NikPRcodes <nikteaching@gmail.com>
2024-09-29 21:45:57 +0530

    DS

--- Data Science/Visualization/ACP.py
+++ Data Science/Visualization/ACP.py
@@ -1,5 +1,5 @@
-from google.colab import drive
-drive.mount('/content/drive/')
+#from google.colab import drive
+#drive.mount('/content/drive/')
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
